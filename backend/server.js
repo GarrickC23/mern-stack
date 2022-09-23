286259e76d34fa5724f8aff23bd555166a51a363
GarrickC23 <garrickchiu0723@gmail.com>
2022-09-23 00:35:56 -0700

    API Routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
+
+//middleware
+app.use(express.json())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
@@ -11,9 +15,8 @@ app.use((req, res, next) => {
 })
 
 //routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
+
 // listen for requests
 app.listen(process.env.PORT, () => {
     console.log('listening on port 4000')
